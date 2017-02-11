41b03e60c586ce3dd319e6a25a328620120c4564
Bryant <bryant.m.wang@gmail.com>
2017-02-10 19:04:59 -0800

    Edit Home Index copy text, add some animations on scroll.

---
+++ GummiBearKingdom/src/GummiBearKingdom/wwwroot/js/scripts.js
@@ -0,0 +1,29 @@
+﻿var $window = $(window);
+var $animation_elements;
+
+function checkView() {
+    var windowHeight = $window.height();
+    var windowTopPosition = $window.scrollTop();
+    var windowBottomPosition = (windowTopPosition + windowHeight);
+
+    $.each($animation_elements, function () {
+        var $element = $(this);
+        var elementHeight = $element.outerHeight();
+        var elementTopPosition = $element.offset().top;
+        var elementBottomPosition = (elementTopPosition + elementHeight);
+
+        if ((elementBottomPosition >= windowTopPosition) &&
+            (elementTopPosition <= windowBottomPosition)) {
+            $element.addClass('in-view');
+        }
+    });
+}
+
+$(function () {
+    $animation_elements = $(".animation-block");
+
+    $window.on('scroll', checkView);
+    $window.on('scroll resize', checkView);
+
+    $window.trigger('scroll');
+});
